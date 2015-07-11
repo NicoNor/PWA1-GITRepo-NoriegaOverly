1a3eb26de92517f6a8e98f653edb6ff45be3e05e
ThatMoose <norieganicholas88@gmail.com>
2015-07-11 19:18:43 -0400

    Variables and layout set
    Put in variables and functions without code.

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -5,8 +5,39 @@ Assignment: Goal 1: Analyze Duel #1
  */
 
 //self-executing function
-(function(){
+(function() {
 
     console.log("FIGHT!!!");
+
+
+    //player names
+    var playerOneName = "Kabal";
+    var playerTwoName = "Kratos";
+
+    //player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+    //player health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
+
+    var round = 1;
+
+    function fight() {
+        //code will go here
+        winnerCheck()
+    };
+
+
+    function winnerCheck(){
+        //code will go here
+    };
+
+
+    /* Where program gets started */
+    fight();
+
 })();
+
 
